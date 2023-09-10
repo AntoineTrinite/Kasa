be5ba56166237d59ada445166915681874f5d452
Trinité Antoine <antoine.trinite@gmail.com>
2023-09-10 15:05:50 +0200

    correction bug

--- src/App.js
+++ src/App.js
@@ -3,17 +3,15 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Logements from './pages/Logement';
 import Propos from './pages/Propos';
-import Error404 from './pages/Error404';
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Home />} />
         <Route path="/logements/:id" element={<Logements />} />
         <Route path="/propos" element={<Propos />} />
         {/* 404 error page */}
-        <Route path="*" element={<Error404 />} />
       </Routes>
     </>
   );
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
 createRoot(document.getElementById('root')).render(
   <Router>
-    <Route path="/" component={App} />
+    <App />
   </Router>
 );
